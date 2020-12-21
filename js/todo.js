db5a8e00c11921ffc3d070c853c8365f084390b3
donghwi <ehdehd1289@naver.com>
2020-12-21 18:46:10 +0900

    js - todoList - forEach ++

--- js/todo.js
+++ js/todo.js
@@ -4,30 +4,48 @@ const toDoList = document.querySelector('.js-toDoList');
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
+const saveToDos = () => {
+  //LocalStorage에는 string만 저장
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+};
+
 const paintToDo = (text) => {
   const li = document.createElement('li');
   const delBtn = document.createElement('button');
-  delBtn.innerText = '❌';
   const span = document.createElement('span');
+  const newId = toDos.length + 1;
+  delBtn.innerText = '❌';
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.id = newId
   toDoList.appendChild(li);
-}
+  const toDoObj = {
+    text,
+    newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+};
 
 const handleSumbit = (e) => {
   e.preventDefault();
   const currentValue = toDoInput.value;
   paintToDo(currentValue);
   toDoInput.value = '';
-}
+};
 
 const loadToDos = () => {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (!toDos) {
-
+  const loadedTodos = localStorage.getItem(TODOS_LS);
+  if (loadedTodos) {
+    console.log(loadedTodos)
+    //string으로 바꿧던 내용을 -> Object로 변환
+    const parsedTodos = JSON.parse(loadedTodos)
+    parsedTodos.forEach((toDo) => paintToDo(toDo.text));
   }
-}
+};
 
 loadToDos();
 toDoForm.addEventListener('submit', handleSumbit)
